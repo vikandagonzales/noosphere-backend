fcd62683e57ee3f8e574d25a84b38923ca3a0da8
Vikanda Gonzales <vikanda.gonzales@yahoo.com>
2019-03-16 01:32:40 -0700

    added src/app.js

---
+++ src/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+
+if (process.env.NODE_ENV !== 'production') require('dotenv').load();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// TOOLS
+app.use(cors());
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+// USER ROUTES
+app.use('/auth', require('./routes/auth'));
+
+// DEFAULT ROUTE
+app.use((req, res, next) => {
+  return next({status: 404, message: 'Route not found'});
+});
+
+// ERROR HANDLING
+app.use((err, req, res, next) => {
+  const errorMessage = {};
+  if (process.env.NODE_ENV !== 'production' && err.stack) errorMessage.stack = err.stack;
+  errorMessage.status = err.status || 500;
+  errorMessage.message = err.message || 'Internal Server Error';
+  return res.status(errorMessage.status).send(errorMessage);
+});
+
+// SERVER
+app.listen(port, () => {
+  console.log(`Listening on port ${port} 🐽!`);
+});
